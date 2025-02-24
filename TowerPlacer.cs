3f3a08fbe3452bb0054564aca0e61e8d858d64bc
kamilzed <kamilsw123@gmail.com>
2025-02-24 18:54:01 +0100

    dodanie area w którym dodajesz wieze

--- TowerPlacer.cs
+++ TowerPlacer.cs
@@ -1,12 +1,18 @@
 using Godot;
 using System;
+using System.Net.Http.Headers;
+using System.Threading;
 public partial class TowerPlacer  : Node2D
 {
 	[Export] public PackedScene tower_scn;
 	[Export] public float speed;
 	// Called when the node enters the scene tree for the first time.
+	public CharacterBody2D player;
+	public Area2D areaOfPlacer;
 	public override void _Ready()
 	{
+		player = GetParent().GetNode<CharacterBody2D>("player");
+		areaOfPlacer = GetNode<Area2D>("Area2D");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,11 +25,19 @@ public partial class TowerPlacer  : Node2D
 	}
 		void PlaceTower()
 	{
-		CharacterBody2D tower = tower_scn.Instantiate<CharacterBody2D>();
-		Vector2 position = tower.Position;
-		position.Y = 250;
-		position.X = 415;
-		tower.Position = position;
-		GetTree().Root.AddChild(tower);
+		if(player != null && areaOfPlacer.OverlapsBody(player))
+		{
+			PackedScene scene = ResourceLoader.Load<PackedScene>("res://Tower.tscn");
+			Node instance = scene.Instantiate();
+			AddChild(instance);
+		}
+
+		// CharacterBody2D tower = tower_scn.Instantiate<CharacterBody2D>();
+		// Vector2 position = tower.Position;
+		// position.Y = this.Position.Y;
+		// position.X = this.Position.Y;
+		// tower.Position = position;
+		// AddChild(tower);
+		
 	}
 }
